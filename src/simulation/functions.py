f5a3b8ced3a5cff9a4aab8e7483282764ffb75e3
Paroxyss <paroxysswastaken@gmail.com>
2023-10-07 21:36:30 +0200

    feat: simulation à distance de match (dans le but de les afficher en ssh)

--- src/simulation/functions.py
+++ src/simulation/functions.py
@@ -9,13 +9,13 @@ def draw_player(t, x, y, d):
     else:
         color = BLUE
 
-    pg.draw.circle( screen, color, (x, y), PLAYER_SIZE)
+    pg.draw.circle( screen, color, (x * SCREEN_FACTOR, y * SCREEN_FACTOR), PLAYER_SIZE * SCREEN_FACTOR)
 
-    pg.draw.line( screen, WHITE, (x, y),
+    pg.draw.line( screen, WHITE, (x * SCREEN_FACTOR, y * SCREEN_FACTOR),
         (
-            x + cos(-d) * PLAYER_SIZE,
-            y - sin(-d) * PLAYER_SIZE,
-        ), 2
+            x + cos(-d) * PLAYER_SIZE * SCREEN_FACTOR,
+            y - sin(-d) * PLAYER_SIZE * SCREEN_FACTOR,
+        ), int(2 * SCREEN_FACTOR)
     )
 
 
@@ -31,75 +31,75 @@ def calcy(infos, k, nb_joueurs):
 
 
 def progression_bar(p, max):
-    x = 10
-    y = 10
+    x = 10 * SCREEN_FACTOR
+    y = 10 * SCREEN_FACTOR
 
     for _ in range(4):
-        pg.draw.rect(screen, (255, 255, 255), (x, y, 200, 15), 1)
+        pg.draw.rect(screen, (255, 255, 255), (x, y, 200 * SCREEN_FACTOR, 10 * SCREEN_FACTOR + 5), 1)
 
-    pg.draw.rect(screen, (255, 255, 255), (x, y, 2 * (p / max * 100), 15), 15)
+    pg.draw.rect(screen, (255, 255, 255), (x, y, 2 * (p / max * 100) * SCREEN_FACTOR, 10 * SCREEN_FACTOR + 5), int(10 * SCREEN_FACTOR) + 5)
 
 
 def show_data(infos, k, nb_joueurs):
-    x = infos["pos"]["x"]
-    y = infos["pos"]["y"]
+    x = infos["pos"]["x"] * SCREEN_FACTOR
+    y = infos["pos"]["y"] * SCREEN_FACTOR
     ori = infos["orientation"]
 
     # balle
     ballePos = infos["balle"]["pos"]
     ep = (
-        x + cos(ballePos["angle"] + ori) * ballePos["norme"],
-        y + sin(ballePos["angle"] + ori) * ballePos["norme"]
+        x + cos(ballePos["angle"] + ori) * ballePos["norme"] * SCREEN_FACTOR,
+        y + sin(ballePos["angle"] + ori) * ballePos["norme"] * SCREEN_FACTOR
     )
-    pg.draw.line(screen, WHITE, (x, y), ep, 4)
+    pg.draw.line(screen, WHITE, (x * SCREEN_FACTOR, y * SCREEN_FACTOR), ep, 4 * SCREEN_FACTOR)
 
     # numéro
 
     font = pg.font.Font(None, 24)
 
-    screen.blit(font.render(str(k), True, WHITE), (x + 30, y - 30))
+    screen.blit(font.render(str(k), True, WHITE), ((x + 30) * SCREEN_FACTOR,(y - 30)*SCREEN_FACTOR))
 
     # cage adverse
     cagePos = infos["cage"]
     ep = (
-        x + cos(cagePos["angle"] + ori) * cagePos["norme"],
-        y + sin(cagePos["angle"] + ori) * cagePos["norme"]
+        x + cos(cagePos["angle"] + ori) * cagePos["norme"] * SCREEN_FACTOR,
+        y + sin(cagePos["angle"] + ori) * cagePos["norme"] * SCREEN_FACTOR
     )
 
     pg.draw.line(
         screen,
         LIGHT_BLUE,
-        (x, y),
-        ep,
-        3,
+        (x * SCREEN_FACTOR, y * SCREEN_FACTOR),
+        ep * SCREEN_FACTOR,
+        3 * SCREEN_FACTOR,
     )
 
 
     pg.draw.line(
         screen,
         BLUE,
-        (x, y),
-        (x, y + infos["h"] * (2*(k<=nb_joueurs/2) - 1)),
-        3,
+        (x * SCREEN_FACTOR, y * SCREEN_FACTOR),
+        (x * SCREEN_FACTOR, y + infos["h"] * (2*(k<=nb_joueurs/2) - 1) * SCREEN_FACTOR),
+        3 * SCREEN_FACTOR,
     )
 
     # joueur le plus proche
 
     copainPos = infos["joueurAllie"]["pos"]
     ep = (
-        x + cos(copainPos["angle"] + ori) * copainPos["norme"],
-        y + sin(copainPos["angle"] + ori) * copainPos["norme"]
+        x + cos(copainPos["angle"] + ori) * copainPos["norme"] * SCREEN_FACTOR,
+        y + sin(copainPos["angle"] + ori) * copainPos["norme"] * SCREEN_FACTOR
     )
-    pg.draw.line( screen, PINK, (x, y), ep, 2)
+    pg.draw.line( screen, PINK, (x, y), ep, 2 * SCREEN_FACTOR)
 
     
     advPos = infos["joueurAdverse"]["pos"]
     ep = (
-        x + cos(advPos["angle"] + ori) * advPos["norme"],
-        y + sin(advPos["angle"] + ori) * advPos["norme"]
+        x + cos(advPos["angle"] + ori) * advPos["norme"] * SCREEN_FACTOR,
+        y + sin(advPos["angle"] + ori) * advPos["norme"] * SCREEN_FACTOR
     )
 
-    pg.draw.line( screen, PURPLE, (x, y), ep, 2)
+    pg.draw.line( screen, PURPLE, (x * SCREEN_FACTOR, y * SCREEN_FACTOR), ep, 2 * SCREEN_FACTOR)
 
 
 def draw_goal():
@@ -108,9 +108,9 @@ def draw_goal():
         WHITE,
         (
             0,
-            (SCREEN_HEIGHT - GOAL_SIZE) // 2,
-            GOAL_THICKNESS,
-            GOAL_SIZE,
+            (SCREEN_HEIGHT * SCREEN_FACTOR - GOAL_SIZE * SCREEN_FACTOR) // 2,
+            GOAL_THICKNESS * SCREEN_FACTOR,
+            GOAL_SIZE * SCREEN_FACTOR,
         ),
     )
 
@@ -118,10 +118,10 @@ def draw_goal():
         screen,
         WHITE,
         (
-            SCREEN_WIDTH - GOAL_THICKNESS,
-            (SCREEN_HEIGHT - GOAL_SIZE) // 2,
-            10,
-            GOAL_SIZE,
+            SCREEN_WIDTH * SCREEN_FACTOR - GOAL_THICKNESS * SCREEN_FACTOR,
+            (SCREEN_HEIGHT * SCREEN_FACTOR - GOAL_SIZE * SCREEN_FACTOR) // 2,
+            10 * SCREEN_FACTOR,
+            GOAL_SIZE * SCREEN_FACTOR,
         ),
     )
 
@@ -130,8 +130,8 @@ def draw_ball(x, y):
     pg.draw.circle(
         screen,
         BLACK,
-        (x, y),
-        BALL_SIZE,
+        (x* SCREEN_FACTOR, y* SCREEN_FACTOR),
+        BALL_SIZE * SCREEN_FACTOR,
     )
 
 
@@ -141,30 +141,30 @@ def draw_field():
             screen,
             FIELD_DARK_GREEN if i % 2 == 0 else FIELD_GREEN,
             pg.Rect(
-                i * SCREEN_WIDTH // N_BANDE,
+                int(i * SCREEN_WIDTH * SCREEN_FACTOR)// N_BANDE,
                 0,
-                SCREEN_WIDTH // N_BANDE,
-                SCREEN_HEIGHT,
+                int(SCREEN_WIDTH * SCREEN_FACTOR)// N_BANDE,
+                SCREEN_HEIGHT * SCREEN_FACTOR,
             ),
         )
 
     pg.draw.circle(
         screen,
         WHITE,
-        (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2),
-        100,
-        width=5,
+        (int(SCREEN_WIDTH * SCREEN_FACTOR)// 2, int(SCREEN_HEIGHT * SCREEN_FACTOR)// 2),
+        int(100* SCREEN_FACTOR),
+        width=int(5 * SCREEN_FACTOR),
     )
 
     pg.draw.rect(
         screen,
         WHITE,
-        pg.Rect(SCREEN_WIDTH // 2 - 2.5, 0, 5, SCREEN_HEIGHT),
+        pg.Rect(int(SCREEN_WIDTH * SCREEN_FACTOR)// 2 - 2.5, 0, int(5 * SCREEN_FACTOR), int(SCREEN_HEIGHT * SCREEN_FACTOR)),
     )
 
     pg.draw.circle(
         screen,
         WHITE,
-        (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2),
-        10,
+        (int(SCREEN_WIDTH * SCREEN_FACTOR)// 2, int(SCREEN_HEIGHT* SCREEN_FACTOR)// 2),
+        int(10* SCREEN_FACTOR),
     )

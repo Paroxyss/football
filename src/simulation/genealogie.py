37b5e90db189a1056a8b487ca012275af4c6cd39
vzytoi <65293868+vzytoi@users.noreply.github.com>
2023-10-10 12:48:55 +0200

    feat: affichage

--- src/simulation/genealogie.py
+++ src/simulation/genealogie.py
@@ -1,29 +1,64 @@
 import pygame as pg
-import sys
 import json
-from random import randint
-from time import sleep
+import random
+import colorsys
+from math import fmod
+import random
 
 pg.init()
 running = True
-population_size = 100
+population_size = 20
 screen_width = 1400
 screen_height = 850
 r = screen_width / population_size
 red = (255, 255, 255)
 screen = pg.display.set_mode((screen_width, screen_height))
+ids = {}
+
+
+def read_json():
+    name = "arbreDeVie.json"
+    with open(name) as json_file:
+        data = json.load(json_file)
+        return data
 
 
 def random_color():
-    return (randint(0, 255), randint(0, 255), randint(0, 255))
+    return random.random(), random.random(), random.random()
 
 
-def draw_couche_genealogique(j):
-    global cur_sp
+def average_color(color1, color2):
+    return (
+        (color1[0] + color2[0]) / 2,
+        (color1[1] + color2[1]) / 2,
+        (color1[2] + color2[2]) / 2,
+    )
 
-    for i in range(population_size):
-        color = random_color() if j == 0 else red
-        pg.draw.rect(screen, color, (i * r, r * j, r, r))
+
+def mutate(color):
+    return (
+        fmod(color[0] + random.random() * 0.05, 1),
+        fmod(color[1] + random.random() * 0.05, 1),
+        fmod(color[2] + random.random() * 0.05, 1),
+    )
+
+
+def draw_couche_genealogique(data, j):
+    l = random.sample(data, len(data))
+
+    for i in range(len(l)):
+        if l[i][1] == 0 and l[i][2] == 0:
+            color = random_color()
+        elif l[i][2] == 0:
+            color = mutate(ids[l[i][1]])
+        else:
+            color = mutate(average_color(ids[l[i][1]], ids[l[i][2]]))
+
+        converted = colorsys.hls_to_rgb(color[0], color[1], color[2])
+        converted = (converted[0] * 255, converted[1] * 255, converted[2] * 255)
+        ids[l[i][0]] = color
+
+        pg.draw.rect(screen, converted, (i * r, r * j, r, r))
 
 
 def everage_color(color1, color2):
@@ -34,8 +69,11 @@ def everage_color(color1, color2):
     )
 
 
-for j in range(100):
-    draw_couche_genealogique(j)
+file_content = read_json()
+
+for i in range(len(file_content)):
+    draw_couche_genealogique(file_content[i], i)
+
 
 pg.display.flip()
 

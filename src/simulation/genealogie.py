f01faa3e9c59a6b12b496794e369b7c6dc184154
Paroxyss <paroxysswastaken@gmail.com>
2023-10-11 15:37:43 +0200

    feat: meilleur affichage de la généalogie en couleur

--- src/simulation/genealogie.py
+++ src/simulation/genealogie.py
@@ -5,15 +5,6 @@ import colorsys
 from math import fmod
 import random
 
-pg.init()
-running = True
-population_size = 20
-screen_width = 1400
-screen_height = 850
-r = screen_width / population_size
-red = (255, 255, 255)
-screen = pg.display.set_mode((screen_width, screen_height))
-ids = {}
 
 
 def read_json():
@@ -24,53 +15,76 @@ def read_json():
 
 
 def random_color():
-    return random.random(), random.random(), random.random()
-
-
-def average_color(color1, color2):
-    return (
-        (color1[0] + color2[0]) / 2,
-        (color1[1] + color2[1]) / 2,
-        (color1[2] + color2[2]) / 2,
-    )
+    return random.random(), 0.5 + 0*random.random(), 0.5 #random.random()
 
 
 def mutate(color):
+    while(color[0] < 0):
+        color[0]+=1
+    return color
     return (
-        fmod(color[0] + random.random() * 0.05, 1),
-        fmod(color[1] + random.random() * 0.05, 1),
-        fmod(color[2] + random.random() * 0.05, 1),
+        fmod(color[0] + random.random() * 0.05 - 0.025, 1),
+        min(color[1] + random.random() * 0.05 - 0.025, 1),
+        min(color[2] + random.random() * 0.05 - 0.025, 1),
     )
 
 
 def draw_couche_genealogique(data, j):
-    l = random.sample(data, len(data))
+    l = data#random.sample(data, len(data))
 
+    colors = []
     for i in range(len(l)):
         if l[i][1] == 0 and l[i][2] == 0:
-            color = random_color()
+            color = random_color() #(j/7, 0.5, 0.5) 
         elif l[i][2] == 0:
             color = mutate(ids[l[i][1]])
         else:
             color = mutate(average_color(ids[l[i][1]], ids[l[i][2]]))
 
+        ids[l[i][0]] = color
+        colors.append([color, i])
+
+    def s(a): return fmod(a[0][0] + 1, 1)
+    colors.sort(key=s)
+    i = 0
+    for a in range(len(colors)):
+        color = colors[a][0]
         converted = colorsys.hls_to_rgb(color[0], color[1], color[2])
         converted = (converted[0] * 255, converted[1] * 255, converted[2] * 255)
-        ids[l[i][0]] = color
+        #print(color, converted)
+        pg.draw.rect(screen, converted, (j * r, r * i, r+1, r+1))
+        pg.draw.rect(screen, converted, (j * r, r * (i + len(colors)), r+1, r+1))
+        i += 1
 
-        pg.draw.rect(screen, converted, (i * r, r * j, r, r))
+def circularDistance(a, b):
+    return abs(fmod(abs(b-a), 1))
+# moyenne circulaire pour un cercle entre 0 et 1
+def circlularMean(a, b):
+    m1 = (a+b)/2
+    m2 = m1+0.5
+    if(circularDistance(a, m1) < circularDistance(a, m2)):
+        return m1
+    return m2
 
-
-def everage_color(color1, color2):
+def average_color(color1, color2):
     return (
-        (color1[0] + color2[0]) / 2,
+        circlularMean(color1[0], color2[0]),
         (color1[1] + color2[1]) / 2,
-        (color1[2] + color2[2]) / 2,
+        (color1[2] + color2[2]) / 2
     )
 
 
 file_content = read_json()
 
+pg.init()
+running = True
+population_size = 700 #2*len(file_content)
+screen_width = 1400
+screen_height = 850
+r = screen_width / population_size
+red = (255, 255, 255)
+screen = pg.display.set_mode((screen_width, screen_height))
+ids = {}
 for i in range(len(file_content)):
     draw_couche_genealogique(file_content[i], i)
 

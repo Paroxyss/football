7244c20dba98471f54eb1195c1b9b7988e542b7f
Paroxyss <paroxysswastaken@gmail.com>
2023-11-03 20:50:49 +0100

    feat: genSquare et update de l'affichage en couleurs

--- src/simulation/genealogie.py
+++ src/simulation/genealogie.py
@@ -26,9 +26,8 @@ def mutate(color):
     )
 
 
-def draw_couche_genealogique(data, j):
+def draw_couche_genealogique(surface, data, x):
     l = data#random.sample(data, len(data))
-
     colors = []
     for i in range(len(l)):
         if l[i][1] == 0 and l[i][2] == 0:
@@ -49,8 +48,14 @@ def draw_couche_genealogique(data, j):
         converted = colorsys.hls_to_rgb(color[0], color[1], color[2])
         converted = (converted[0] * 255, converted[1] * 255, converted[2] * 255)
         #print(color, converted)
-        pg.draw.rect(screen, converted, (j * largeur, hauteur * i, largeur+1, hauteur+1))
+        pg.draw.rect(surface, converted, (x, i, 1, 1))
         i += 1
+
+def make_arbre_genealogique(data):
+    surface = pg.Surface((len(data), len(data[0])))
+    for i in range(len(data)):
+        draw_couche_genealogique(surface, data[i], i)
+    return surface
 
 def getCol(i):
     return lambda x: x[i]
@@ -58,20 +63,20 @@ def getCol(i):
 def verticalGet(d,i):
     return np.fromiter(map(getCol(i), d), float)
 
-def draw_stats(data):
-    maxes = [
-        np.max(verticalGet(data, 0)),
-        np.max(verticalGet(data, 1)),
-        np.max(verticalGet(data, 2)),
-        np.max(verticalGet(data, 3)),
-        np.max(verticalGet(data, 4)),
-    ]    
+def make_stats(data):
+    res = 1000
+    wres = 3
+    surface = pg.Surface((len(data)*wres, res))
+    lineWidth = int(len(data)/1000)
+    
     colors = [(0,0,0),(0,255,0),(255,0,0),(255,255,0),(0,255,255)]
+    
     for i in range(len(data)-1):
-        for d in range(len(data[i])):
+        for d in range(1,len(data[i])):
             def getDataAt(i):
-                return (2-data[i][d]/maxes[d])*(screen_height/2)
-            pg.draw.line(screen, colors[d], ((i+1)*largeur, getDataAt(i)), ((i+2)*largeur, getDataAt(i+1)), 2)
+                return (1-data[i][d]/maxes[d])*res
+            pg.draw.line(surface, colors[d], (i*wres, getDataAt(i)), ((i+1)*wres, getDataAt(i+1)), lineWidth)
+    return surface
     
 def circularDistance(a, b):
     return abs(fmod(abs(b-a), 1))
@@ -93,25 +98,118 @@ def average_color(color1, color2):
 file_content = read_json()
 
 genes=file_content["genealogy"]
+stats = file_content["stats"]
 
-pg.init()
+screen_width = 1400
+screen_height = 650
+
+
 running = True
 n_gen = len(genes)
 population_size = len(genes[0])
-screen_width = 1400
-screen_height = 650
 hauteur = screen_height/population_size/2
 largeur = screen_width/n_gen
 red = (255, 255, 255)
 screen = pg.display.set_mode((screen_width, screen_height))
 ids = {}
-for i in range(len(genes)):
-    draw_couche_genealogique(genes[i], i)
-draw_stats(file_content["stats"])
+maxes = [
+    np.max(verticalGet(stats, 0)),
+    np.max(verticalGet(stats, 1)),
+    np.max(verticalGet(stats, 2)),
+    np.max(verticalGet(stats, 3)),
+    np.max(verticalGet(stats, 4)),
+]
 
-pg.display.flip()
+pg.init()
 
+arbre = make_arbre_genealogique(genes)
+stats = make_stats(stats)
+
+displaySegment = (0, len(genes))
+displayRatio = 1/2
+def displayD(startG, endG):
+    global displaySegment
+    trajet = (startG, endG) if (startG < endG) else (endG, startG)
+    displaySegment = (trajet[0], trajet[1])
+    print(displaySegment)
+    
+    subArbre = pg.Surface((trajet[1] - trajet[0], arbre.get_height()))
+    subArbre.blit(arbre, (-trajet[0], 0))
+    subArbre = pg.transform.scale(subArbre, (screen_width, screen_height*displayRatio))
+    
+    subStats = pg.Surface(((trajet[1] - trajet[0]) * stats.get_width() / arbre.get_width(), stats.get_height()))
+    subStats.blit(stats, (-trajet[0] * stats.get_width() / arbre.get_width(), 0))
+    subStats = pg.transform.smoothscale(subStats, (screen_width, screen_height*(1-displayRatio)))
+    
+    screen.fill((0,0,0))
+    screen.blit(subArbre, (0,0))
+    screen.blit(subStats, (0,screen_height*displayRatio))
+
+startPos = (0,0)
+endPos = (len(genes),0)
+
+
+displayD(0, len(genes))
+zoomPile = []
+
+def regenaAll():
+    global ids
+    global arbre
+    ids = {}
+    arbre = make_arbre_genealogique(genes)
+    displayD(displaySegment[0], displaySegment[1])
+
+mvspeed = 0.03
+clock = pg.time.Clock()
 while running:
+    pg.display.flip()
+    clock.tick(20)
+  
+    pressed = pg.key.get_pressed()
+    zFactor = displaySegment[1]-displaySegment[0]
+    if pressed[pg.K_h]:
+        displaySegment = (displaySegment[0] - mvspeed*zFactor, displaySegment[1] - mvspeed*zFactor)
+        displayD(displaySegment[0], displaySegment[1])
+    if pressed[pg.K_l]:
+        displaySegment = (displaySegment[0] + mvspeed*zFactor, displaySegment[1] + mvspeed*zFactor)
+        displayD(displaySegment[0], displaySegment[1])
+    if pressed[pg.K_j]:
+        displaySegment = (displaySegment[0] - mvspeed*zFactor, displaySegment[1] + mvspeed*zFactor)
+        displayD(displaySegment[0], displaySegment[1])
+    if pressed[pg.K_k]:
+        displaySegment = (displaySegment[0] + mvspeed*zFactor, displaySegment[1] - mvspeed*zFactor)
+        displayD(displaySegment[0], displaySegment[1])
+        
     for event in pg.event.get():
+        print(event)
         if event.type == pg.QUIT:
             running = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            startPos = event.pos
+        elif event.type == pg.MOUSEBUTTONUP:
+            endPos = event.pos
+            lengthFactor = (displaySegment[1] - displaySegment[0])/screen_width
+            zoom = (
+                startPos[0]*lengthFactor+displaySegment[0], 
+                endPos[0]*lengthFactor+displaySegment[0]
+            )
+            zoomPile.append(displaySegment)
+            displaySegment = zoom
+            displayD(zoom[0], zoom[1])
+        elif event.type == pg.KEYDOWN and event.key == 8:
+            if len(zoomPile) == 0:
+                displayD(0, len(genes))
+            else:
+                zoom = zoomPile.pop()
+                displayD(zoom[0], zoom[1])
+        elif event.type == pg.KEYDOWN and event.unicode == 'r':
+            regenaAll()
+        # 81: flèche bas, 82: flèche haut
+        elif event.type == pg.KEYDOWN and event.scancode == 81:
+            displayRatio += 0.1
+            displayRatio = min(1, displayRatio)
+            displayD(displaySegment[0], displaySegment[1])
+        elif event.type == pg.KEYDOWN and event.scancode == 82:
+            displayRatio -= 0.1
+            displayRatio = max(0, displayRatio)
+            displayD(displaySegment[0], displaySegment[1])
---
+++ src/simulation/genesSquare.py
@@ -0,0 +1,160 @@
+import pygame as pg
+import numpy as np
+import sys
+import json
+import random
+import colorsys
+from math import fmod, pow
+import random
+
+def read_json():
+    name = "stats.json"
+    with open(name) as json_file:
+        data = json.load(json_file)
+        return data
+
+def circularDistance(a, b):
+    return min(fmod(abs(b-a)+1, 1), fmod(b+a, 1))
+# moyenne circulaire pour un cercle entre 0 et 1
+def circlularMean(a, b):
+    m1 = (a+b)/2
+    m2 = m1+0.5
+    #print(a, m1, circularDistance(a, m1))
+    #print(a, m2, circularDistance(a, m1))
+    if(circularDistance(a, m1) < circularDistance(a, m2)):
+        return m1
+    return m2
+
+def average_color(color1, color2):
+    return (
+        (color1[0] + color2[0]) / 2,
+        min(max(color1[1], color2[1]) + 0.0075, 1),
+        (color1[2] + color2[2])/2
+    )
+
+def random_color():
+    return 0.1,0,1#random.random(), 0.5 + 0*random.random(), 0.5 #random.random()
+
+def mutate_color(color):
+    #return color
+    return (
+        fmod(color[0]+pow(1-color[0], 2)/15, 1),#fmod(color[0] + random.random() * 0.05 - 0.025 + 1, 1),
+        color[1],#min(color[1]+0.005, 0.9),#min(color[2] + random.random() * 0.05 - 0.025, 1),
+        color[2],#min(color[1] + random.random() * 0.05 - 0.025, 1),
+    )
+
+def average_pos(p1, p2):
+    return (
+        circlularMean(p1[0], p2[0]),
+        circlularMean(p1[1], p2[1])
+    )
+
+def random_position():
+    return random.random(), random.random()
+ 
+def mutate_pos(pos):
+    return (
+        fmod(pos[0] + random.random() * 0.02 - 0.01 + 1, 1),
+        fmod(pos[1] + random.random() * 0.02 - 0.01 + 1, 1),
+    )
+
+def draw_couche_genealogique(surface, data):
+    l = data
+    idToPrint = []
+    for i in range(len(l)):
+        id = l[i][0]
+        p1 = l[i][1]
+        p2 = l[i][2]
+        
+        if p1 == 0 and p2 == 0:
+            color = random_color() #(j/7, 0.5, 0.5) 
+            pos = random_position()
+        elif p2 == 0:
+            color = mutate_color(colors[p1])
+            pos = mutate_pos(coords[p1])
+        else:
+            color = mutate_color(average_color(colors[p1], colors[p1]))
+            pos = mutate_pos(average_pos(coords[p1], coords[p2]))
+
+        colors[id] = color
+        coords[id] = pos
+
+        if p1 == 0 and p2 == 0:
+            pass
+        
+        idToPrint.append(id)
+
+    def s(a): return colors[a][1]
+    idToPrint.sort(key=s)
+    for i in range(len(idToPrint)):
+        id = idToPrint[i]
+        color = colors[id]
+        pos = coords[id]
+        
+        converted = colorsys.hls_to_rgb(color[0], color[1], color[2])
+        converted = (converted[0] * 255, converted[1] * 255, converted[2] * 255)
+        pg.draw.rect(surface, converted, (pos[0]*surface.get_width(), pos[1]*surface.get_height(), 5, 5))
+
+for i in range(0, 90):
+    p2 = (i/100, 0)
+    p1 = (i/100+0.1, 0)
+    print(average_pos(p1, p2))
+#sys.exit()
+
+file_content = read_json()
+
+genes=file_content["genealogy"]
+stats = file_content["stats"]
+
+screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+opacityMask = pg.Surface((screen.get_width(), screen.get_height()))
+opacityMask.fill((0,0,0))
+opacityMask.set_alpha(1)
+
+colors = {}
+coords = {}
+
+pg.init()
+
+clock = pg.time.Clock()
+
+
+dx = 41
+dy = 30
+npersquare = 1
+sqSize = screen.get_width()/dx
+
+def drawAll():
+    screen.fill(0)
+    colors = {}
+    coords = {}
+    for y in range(dy):
+        for x in range(dx):
+            genR = pg.Surface((100,100))
+            for i in range(npersquare):
+                draw_couche_genealogique(genR, genes[dx*npersquare*y + npersquare*x + i])
+            genR = pg.transform.scale(genR, (sqSize, sqSize))
+            screen.blit(genR, (x*sqSize, y*sqSize))
+        pg.display.flip()
+
+running = True
+animationI = 0;
+animation = False
+drawAll()
+while running:
+    clock.tick(50)
+    if(animation):
+        draw_couche_genealogique(screen, genes[animationI])
+        pg.display.flip()
+        screen.blit(opacityMask, (0,0))
+        animationI+=1
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.KEYDOWN and event.unicode == 'r':
+            animation = False
+            drawAll()
+        elif event.type == pg.KEYDOWN and event.unicode == 'p':
+            animationI=0
+            animation = True
+            screen.fill((0,0,0))
